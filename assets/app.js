b6a3fb4a71b49769587a8d43895b122ab38226ea
AyoubBalti <ayoub.balti77@gmail.com>
2022-01-26 18:54:25 +0100

    Template

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,12 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+import { Tooltip,Toast,Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+ 
+//******************************************************************************************* */
+//CustomJs
